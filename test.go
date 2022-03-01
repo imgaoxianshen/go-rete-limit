b5cddadb0885bae0472e559051adbdfffdfd6d63
imgaoxianshen <690220607@qq.com>
2022-03-01 16:46:58 +0800

    Create test.go
    change

--- test.go
+++ test.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	cache := lib.NewGCache(lib.WithMaxSize(3))
-	cache.Set("name", "shenyi", time.Second * 3)
+	cache.Set("name", "gaofeng", time.Second * 3)
 	cache.Set("age", 0, 0)
 	for {
 		fmt.Printf("name=%v,age=%v",cache.Get("name"), cache.Get("age"))
